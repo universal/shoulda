df3317251beb2092dfb74489ebba65014180ad85
universaL <jhedtrich@gmail.com>
2010-03-08 15:12:38 +0100

    changed translation loading to be in line with new rails3 namespacing

--- lib/shoulda/active_record/helpers.rb
+++ lib/shoulda/active_record/helpers.rb
@@ -17,7 +17,11 @@ module Shoulda # :nodoc:
       #   default_error_message(:too_long, :count => 60)
       def default_error_message(key, values = {})
         if Object.const_defined?(:I18n) # Rails >= 2.2
-          I18n.translate("activerecord.errors.messages.#{key}", values)
+          if key == :taken || key == :record_invalid
+            I18n.translate("activerecord.errors.messages.#{key}", values)
+          else
+            I18n.translate("errors.messages.#{key}", values)
+          end
         else # Rails <= 2.1.x
           ::ActiveRecord::Errors.default_error_messages[key] % values[:count]
         end
